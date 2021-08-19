6faf9d26ef1624649ef7d6fd8dc05dda3a2d0b91
Anton TJ <antje12@student.sdu.dk>
2021-08-19 10:57:11 +0000

    Changed workflow for test V10

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-echo "This is a thing: $1"
+echo "This is a thing: ${GIT_COMMIT}"
 docker build -t "${DockerRepo}$ENV_NAME:latest" -t "${DockerRepo}$ENV_NAME:1.0-$Tag" ./"$ENV_NAME"
