f73858e001e720f27faac7021d18bd4578866b2f
Tobias Kristensen <tokri19@student.sdu.dk>
2021-08-21 17:17:05 +0000

    Improve Kubernetes health check script to accept delay and port arguments

--- scripts/kubernetes-test.sh
+++ scripts/kubernetes-test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash 
 MAX_ATTEMPTS=30
 CURRENT_ATTEMPT=1
+DELAY=${1:-0}
+PORT=${2:-0}
+
+echo "Starting up - Delay: ${DELAY} seconds"
+sleep $DELAY
 
 while [ $CURRENT_ATTEMPT -le $MAX_ATTEMPTS ]; do
     echo "Testing connection - Attempt: ${CURRENT_ATTEMPT} of ${MAX_ATTEMPTS}"
-    RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" http://34.79.255.98:32540/)
+    RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" http://34.79.255.98:${PORT}/)
     
     if [ $RESULT -ne '200' ]; then
      echo "Connection didn't return a status code of 200. Connection failed"
